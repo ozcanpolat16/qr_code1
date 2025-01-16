1d6efd7bf543de83886d10da0799f449b189b5b4
ozcanpolat <ozcanp132@gmail.com>
2025-01-16 05:17:55 +0300

    Son değişiklikler yapıldı

--- generate_qr.py
+++ generate_qr.py
@@ -1,12 +1,12 @@
-import qrcode
+# import qrcode
 
-# Flask API URL'si
-url = "http://127.0.0.1:5000/"
+# # Flask API URL'si
+# url = "http://127.0.0.1:5000/"
 
-# QR kodunu oluştur
-qr = qrcode.make(url)
+# # QR kodunu oluştur
+# qr = qrcode.make(url)
 
-# QR kodunu kaydet
-qr.save("dynamic_qr_code.png")
+# # QR kodunu kaydet
+# qr.save("dynamic_qr_code.png")
 
-print("QR kodu oluşturuldu ve kaydedildi: dynamic_qr_code.png")
+# print("QR kodu oluşturuldu ve kaydedildi: dynamic_qr_code.png")
